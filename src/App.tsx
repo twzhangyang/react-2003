e07d1229ee43bcc45e03a44b7ad3d64a2dd41c07
Yang <yrzhang@thoughtworks.com>
2023-11-13 09:51:35 +1100

    create pages folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,110 +1,13 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import "./App.css";
-import {Logo, Navbar, Search, SearchResult} from "./components/Navbar";
-import {MovieList, MoviePanel} from "./components/MoviePanel";
-import {WatchedMoviePanel} from "./components/WatchedMoviePanel";
-import Loading from "./components/Loader";
-import MovieDetails, {watchedMovieModel} from "./components/MovieDetails";
-import {useLocalStorageState} from "./hooks/useLocalStorageState";
-import {useMovies} from "./hooks/useMovies";
-
+import Popcorn from "./pages/Popcorn";
 export default function App() {
-
-    const [query, setQuery] = useState('');
-    // const [movies, setMovies] = useState<MovieModel[]>([]);
-    // const [isLoading, setIsLoading] = useState(false);
-    // const [error, setError] = useState('');
-    const { movies, isLoading, error } = useMovies(query);
-    const [selectedId, setSelectedId] = useState<null | string>(null);
-    // const [watched, setWatched] = useState<watchedMovieModel[]>([])
-    const [watched, setWatched] = useLocalStorageState<watchedMovieModel[]>([], "watched");
-
-    const handleSelectMovie = (id: string) => {
-        setSelectedId((selectedId) => id === selectedId ? null : id)
-    }
-
-    const handleCloseMovie = () => {
-        setSelectedId(null);
-    }
-
-    const handleAddWatched = (movie: watchedMovieModel) => {
-        setWatched((watched: watchedMovieModel[]) => [...watched, movie])
-    }
-
-    const handleDeletedWatched = (id: string) => {
-        setWatched((watched) => watched.filter((movie) => movie.imdbID !== id))
-    }
-
-    // useEffect(() => {
-    //     setIsLoading(true);
-    //     setError('')
-    //
-    //     const fetchMovies = async () => {
-    //         try {
-    //             const result = await fetch(`http://www.omdbapi.com/?apikey=${APIKey}&s=${query}`)
-    //             if (!result.ok) {
-    //                 throw new Error('Something went wrong with fetching movies')
-    //             }
-    //             const json = await result.json();
-    //
-    //             if (json.Response === 'False' && json.Error === 'Incorrect IMDb ID.') {
-    //                 return;
-    //             }
-    //
-    //             if (!json.Search) {
-    //                 throw new Error('Movie not found');
-    //             }
-    //             setMovies(json.Search);
-    //         } catch (e: any) {
-    //             if (e.name !== 'AbortError') {
-    //                 setError(e.message);
-    //             }
-    //         } finally {
-    //             setIsLoading(false);
-    //         }
-    //     }
-    //
-    //     if (query.length < 3) {
-    //         setMovies([]);
-    //     }
-    //
-    //     fetchMovies();
-    // }, [query]);
-
     return (
         <>
-            <Navbar>
-                <Logo></Logo>
-                <Search query={query} setQuery={setQuery}></Search>
-                <SearchResult movies={movies}></SearchResult>
-            </Navbar>
-            <main className="main">
-                {/*<MoviePanel>*/}
-                {/*    <MovieList movies={tempMovieData}/>*/}
-                {/*</MoviePanel>*/}
-                {error && <ErrorMessage message={error}/>}
-                {isLoading && <Loading></Loading>}
-                {!error && !isLoading && <MoviePanel element={
-                    <>
-                        <MovieList movies={movies} onSelectMovie={handleSelectMovie}/>
-                    </>
-                }/>}
-                {selectedId && <MovieDetails selectedId={selectedId} onCloseMovie={handleCloseMovie}
-                                             onAddWatched={handleAddWatched} watched={watched}/>}
-
-                {!selectedId && <WatchedMoviePanel watched={watched}></WatchedMoviePanel>}
-            </main>
+          <Popcorn></Popcorn>
         </>
     );
 }
 
-
-const ErrorMessage: React.FC<{ message: string }> = ({message}) => {
-    return (
-        <p className='error'>
-            <span>Error:</span> {message}
-        </p>
-    )
-}
 
 
---
+++ src/pages/Product.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Product = () => (
+   <p>This is product page</p>
+);
+
+export default Product;
