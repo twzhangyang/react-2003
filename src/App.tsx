54cae8a264864b1d70a49e139b541f9cdafe5f12
Yang <yrzhang@thoughtworks.com>
2023-11-13 09:57:51 +1100

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
 import Popcorn from "./pages/Popcorn";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+
 export default function App() {
     return (
-        <>
-          <Popcorn></Popcorn>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="popcorn" element={<Popcorn/>}></Route>
+                <Route path="/" element={<Home/>}></Route>
+                <Route path="product" element={<Product/>}></Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
