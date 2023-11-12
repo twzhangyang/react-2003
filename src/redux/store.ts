09f46bc40d9946a4846e1cb0c662569e96addba6
Yang <yrzhang@thoughtworks.com>
2023-11-13 10:49:53 +1100

    integrate redux dev tool and thunk middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,14 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import {customerReducer} from "../components/bank/customers/customerSlice";
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
     customer: customerReducer
 })
 
-export const store = createStore(rootReducer);
+// 1. Manually create store
+// export const store = createStore(rootReducer);
 
 // console.log(store.getState());
 // store.dispatch({
@@ -18,3 +21,8 @@ export const store = createStore(rootReducer);
 // })
 // console.log(store.getState());
 
+// 2. Manually create store and integrate with redux dev tool and thunk middleware
+export const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
