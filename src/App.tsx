61f6db5a932345e7fff8a818cf821d20f524503e
Yang <yrzhang@thoughtworks.com>
2023-11-13 10:00:37 +1100

    add not found

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,22 @@ import Popcorn from "./pages/Popcorn";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="popcorn" element={<Popcorn/>}></Route>
-                <Route path="/" element={<Home/>}></Route>
-                <Route path="product" element={<Product/>}></Route>
-            </Routes>
-        </BrowserRouter>
+        <div>
+            <h1>Hello, React</h1>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="popcorn" element={<Popcorn/>}></Route>
+                    <Route path="/" element={<Home/>}></Route>
+                    <Route path="product" element={<Product/>}></Route>
+                    <Route path="*" element={<NotFound></NotFound>} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+
     );
 }
 
